3bb5589872c52104da3a3f7aaa9faf73baa808c2
Marni Hager <mlh6118@gmail.com>
2022-08-11 18:41:01 -0700

    Adding tests for failures.

--- django_snacks/tests.py
+++ django_snacks/tests.py
@@ -15,6 +15,19 @@ class SnacksTestPages(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class SnacksTestPageFailures(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_about_page_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, 500)
+
+
 class SnacksTestTemplates(SimpleTestCase):
     def test_home_page_template(self):
         url = reverse('home')
